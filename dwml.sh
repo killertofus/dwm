fe4ae68a1a343b458e952526b53801705e6f0a47
killertofus <cinemassacres@gmail.com>
2023-08-26 22:15:42 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -94,7 +94,7 @@ wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee 
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 sudo apt update
 sudo apt install github-desktop
-sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
+sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove -y
 ./gwml.sh
 cd config/debian
 mv .zshrc ~/.
