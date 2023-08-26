1620236875a27b2f59c3b16dae92b556ab8bdd73
killertofus <cinemassacres@gmail.com>
2023-08-26 22:15:08 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -40,10 +40,17 @@ sudo apt install librewolf -y
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/jellyfin.gpg
+cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
+Types: deb
+URIs: https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )
+Suites: $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )
+Components: main
+Architectures: $( dpkg --print-architecture )
+Signed-By: /etc/apt/keyrings/jellyfin.gpg
+EOF
 sudo apt update
-sudo apt install jellyfin -y
+sudo apt install jellyfin
 sudo systemctl {action} jellyfin
-
 
 sudo mv streamlink.desktop /usr/share/applications
 sudo mv chatterino.desktop /usr/share/applications
