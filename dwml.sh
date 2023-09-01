2c3ffb15892b222c7a7aaaca2e662a94a632c603
killertofus <cinemassacres@gmail.com>
2023-09-01 21:38:58 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository ppa:aslatter/ppa -y
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/ lsb_release -c .sources
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/*/*.sources
 sudo apt update
 sudo apt install winehq-staging -y
 sudo apt install alacritty -y
