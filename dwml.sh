39aa5b80d38034eac4bde77739a944aa03f825be
killertofus <cinemassacres@gmail.com>
2023-09-01 23:59:19 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -51,7 +51,7 @@ sudo apt update
 sudo apt install nodejs -y
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-curl https://repo.jellyfin.org/install-debuntu.sh |echo -ne '\n' | sudo bash
+curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash | sleep 2 echo -ne '\n' 
 sudo mv streamlink.desktop /usr/share/applications
 sudo mv chatterino.desktop /usr/share/applications
 sudo mv rustdesk.desktop /usr/share/applications
