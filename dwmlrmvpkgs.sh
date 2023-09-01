f4da275506edcf1a47b1fb2085bd2f7fbce833ca
killertofus <cinemassacres@gmail.com>
2023-09-01 19:14:58 +0000

    Update dwmlrmvpkgs.sh

--- dwmlrmvpkgs.sh
+++ dwmlrmvpkgs.sh
@@ -1,5 +1,5 @@
 # please don't run this more then twice :)
-pkgToRemoveListFull=" emacsen-common dictionaries-common transmission-common telnet $DESKTOP_SESSION  drawing baobab slick-greeter gnome-terminal webapp-manager mint-upgrade-info hexchat-common gnome-calculator celluloid gnome-games gnome-terminal hypnotix nemo hexchat orca xterm nano transmission libreoffice-base-core libreoffice-common p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny xterm warpinator youtube-dl yelp 'gnome*' 'mint*'"
+pkgToRemoveListFull=" emacsen-common dictionaries-common transmission-common telnet $DESKTOP_SESSION  mintreport drawing baobab slick-greeter gnome-terminal webapp-manager mint-upgrade-info hexchat-common gnome-calculator celluloid gnome-games gnome-terminal hypnotix nemo hexchat orca xterm nano transmission libreoffice-base-core libreoffice-common p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny xterm warpinator youtube-dl yelp 'gnome*' 'mint*'"
 pkgToRemoveList=""
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(dpkg --status $pkgToRemove &> /dev/null)
