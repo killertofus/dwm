c36e422020ede81ccd95d8054a6a0036c59c75cd
killertofus <cinemassacres@gmail.com>
2023-09-01 18:49:00 +0000

    added the latest version of node js to the installer

--- dwml.sh
+++ dwml.sh
@@ -35,6 +35,14 @@ EOF
 sudo apt update
 
 sudo apt install librewolf -y
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+sudo apt update
+sudo apt install nodejs -y
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 yes "" | curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 
