6e621bd19f3a4e76eeb0af8c74a164a0e173c0b4
killertofus <cinemassacres@gmail.com>
2023-09-08 02:04:16 +0000

    new version of nodejs will come

--- dwml.sh
+++ dwml.sh
@@ -50,12 +50,6 @@ sudo apt update
 sudo apt install librewolf -y
 sudo apt update
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-sudo apt update
-sudo apt install nodejs -y
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo mv streamlink.desktop /usr/share/applications
 sudo mv chatterino.desktop /usr/share/applications
