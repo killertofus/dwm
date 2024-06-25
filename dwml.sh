98821951527c7148b0ce530836e6cc7c434058cb
killertofus <cinemassacres@gmail.com>
2024-06-25 17:56:54 +0000

    typo, I lost

--- dwml.sh
+++ dwml.sh
@@ -105,7 +105,7 @@ echo "# ZVM" >> $HOME/.profile
 echo export ZVM_INSTALL="$HOME/.zvm/self" >> $HOME/.profile
 echo export PATH="$PATH:$HOME/.zvm/bin" >> $HOME/.profile
 echo export PATH="$PATH:$ZVM_INSTALL/" >> $HOME/.profile
-zug i master
+zig i master
 git clone --recurse-submodules https://github.com/fairyglade/ly
 cd ly
 zig build
