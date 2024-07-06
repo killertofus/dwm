c8376190e07ff418b4da6be5cd9fdcfe684790b4
killertofus <cinemassacres@gmail.com>
2024-07-06 20:52:36 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "RANGER_LOAD_DEFAULT_RC=false"
-cp -r .themes ~/
-mv .config ~/
+mv .themes ~/
+cp -r .config ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
