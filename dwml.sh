2d81100f0936a2e82534ca662a1ea883fca9e084
killertofus <cinemassacres@gmail.com>
2024-07-13 02:55:22 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -17,7 +17,7 @@ do
             sed -i '74 a  echo git clone https://github.com/LGFae/swww/' dwml.sh
             sed -i '75 a echo cd sww ' dwml.sh
             sed -i '76 a sleep 6; pwd ' dwml.sh
-            sed -i '2,28d' dwml.sh && exit && ./dwml.sh
+            sed -i '2,28d' dwml.sh && exit &&  exec ./dwml.sh
             ;;
         "dwm")
             echo "you chose choice 2"
