4e27bd45b240db4f34b40c38aa24d705312680b9
killertofus <cinemassacres@gmail.com>
2024-07-13 00:18:22 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -6,6 +6,7 @@ mv .icons ~/
 sudo mv dwm.desktop /usr/share/xsessions/
 mv .xinitrc ~/
 cp -r .config ~/
+mv .zshrc ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
