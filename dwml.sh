4eb52dd616a6dd57d6e436e3e2defaf717550393
killertofus <cinemassacres@gmail.com>
2024-07-14 02:21:33 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -21,7 +21,7 @@ do
             break  # exit the loop after dwl is chosen
             ;;
         "dwm")
-            break
+            done
             ;;
         *) echo "invalid option $REPLY";;
     esac
