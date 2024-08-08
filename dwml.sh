3a6d9674def85a8f26d05af930d1f16dacb324d4
killertofus <cinemassacres@gmail.com>
2024-08-08 20:16:46 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -4,9 +4,9 @@ sudo wget -p /usr/share/fonts/Iosevka
 sudo wget  https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka
 sudo tar -xvf /usr/share/fonts/Iosevka/Iosevka.tar.xz -C /usr/share/fonts/Iosevka/
 sudo rm -rf /usr/share/fonts/Iosevka/Iosevka.tar.xz /usr/share/fonts/Iosevka/*.md
-sudo apt update && sudo apt install nala -y
+sudo apt update -y
 sudo nala fetch --auto
-xargs sudo nala install <dwmlpkgs.txt -y
+xargs sudo apt install <dwmlpkgs.txt -y
 sudo -v
 sleep 0.6; xdotool key 'Return' | curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 sudo dpkg --add-architecture i386
@@ -20,11 +20,11 @@ main() {
 }
 
 main
-sudo nala update && sudo nala upgrade -y
+sudo apt update && sudo apt upgrade -y
 sudo dpkg --configure -a
 sudo -v
 echo 'Defaults !pwfeedback'|sudo tee /etc/sudoers.d/9_no_pwfeedback
-sudo nala install winehq-staging -y
+sudo apt install winehq-staging -y
 sudo -v
 chsh -s $(which zsh)
 fc-cache -f -v
@@ -43,8 +43,8 @@ Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
 sudo apt update
 
-sudo nala install librewolf -y
-sudo nala update
+sudo apt install librewolf -y
+sudo apt update
 
 
 mkdir zig
@@ -99,9 +99,9 @@ sudo mv rustdesk Chatterino Streamlink_Twitch_GUI /usr/local/bin
 
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
-sudo nala update
-sudo nala install github-desktop
-sudo nala nala && sudo nala upgrade && sudo nala clean && sudo nala autoclean && sudo nala autoremove -y
+sudo apt update -y
+sudo apt install github-desktop -y
+sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove -y
 ./gwml.sh
 ./Jdownloader2.sh
 git clone --recurse-submodules https://github.com/fairyglade/ly
