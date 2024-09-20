978c631f97cd215ffba15eb9d8e9b86aa683936c
killertofus <cinemassacres@gmail.com>
2024-09-20 20:08:37 +0000

    hey winking face emoji

--- dwmlrmvpkgs.sh
+++ dwmlrmvpkgs.sh
@@ -1,6 +1,6 @@
 # please don't run this more then twice :)
 pkgToRemoveListFull=""
-pkgToRemoveList="emacsen-common dictionaries-common xterm transmission-common telnet xdotool x-terminal-emulator mintreport xviewer drawing baobab slick-greeter '*cinnamon*-' gnome-terminal webapp-manager mint-upgrade-info hexchat-common gnome-calculator celluloid gnome-games gnome-terminal hypnotix nemo hexchat orca xterm nano transmission libreoffice-base-core libreoffice-common p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny xterm warpinator youtube-dl yelp 'mint*'"
+pkgToRemoveList="emacsen-common dictionaries-common xterm transmission-common telnet x-terminal-emulator mintreport xviewer drawing baobab slick-greeter *cinnamon-* gnome-terminal webapp-manager mint-upgrade-info hexchat-common gnome-calculator celluloid gnome-games gnome-terminal hypnotix nemo hexchat orca xterm nano transmission *libreoffice-*  p7zip-full *rhythmbox-* vim-common vim-tiny xterm warpinator yt-dlp yelp"
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(dpkg --status $pkgToRemove &> /dev/null)
   if [[ $? -eq 0 ]]; then
