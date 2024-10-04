f2e53b40a95efdda4dda3bc9ab88c17bbc9b0cf2
killertofus <cinemassacres@gmail.com>
2024-10-04 17:53:40 -0600

    I punched a tree to make this commit

--- dwml.sh
+++ dwml.sh
@@ -42,6 +42,12 @@ wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
  sudo mv zig/lib zig/zig /usr/local/bin
  cd -
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+
 
 flatpak install -y --noninteractive flathub com.chatterino.chatterino/x86_64/stable io.github.shiftey.Desktop org.jellyfin.JellyfinServer JDownloader
 sudo mv streamlink.desktop /usr/share/applications
