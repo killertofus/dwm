1d7663ec8e675f622bc1e5c1892a02abb113ebf2
killertofus <cinemassacres@gmail.com>
2025-01-10 21:13:50 -0700

    I don't know.

--- configs.sh
+++ configs.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ZSH="$XDG_CONFIG_HOME/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 mv themes ~/.local/share
 cp -r .config ~/
-mv icons ~/.local/share
 mv .zshrc ~/
+mv icons ~/.local/share
 sudo mkdir -p /usr/share/xsessions
 sudo mv dwm.desktop /usr/share/xsessions/
 mv .xinitrc ~/
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
-gsettings set org.nemo.preferences show-hidden-files-true
+gsettings set org.nemo.preferences show-hidden-files true
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
-mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
-rm -rf master.zip
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.config/oh-my-zsh/themes
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
 make -C ~/.config/dwm/ clean
 sudo make -C ~/.config/dwm/ clean install
